a2fe94831aa7e4f127fd907620d14bd4a41a11d5
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-03-06 15:13:06 +0300

    added connection to database

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const start = async () => {
+    try {
+        // Подключение к БД
+        await sequelize.authenticate();
+        await sequelize.sync(); //Сверяет состояние таблиц в коде и в БД
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
